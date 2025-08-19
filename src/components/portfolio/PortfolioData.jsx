9ccf857fdca130b5243510a12d34820d0c9a1497
Edward Ogheneochuko <headiesed@gmail.com>
2025-08-18 17:33:59 -0700

    _

--- src/components/portfolio/PortfolioData.jsx
+++ src/components/portfolio/PortfolioData.jsx
@@ -4,16 +4,24 @@ import Terra from '../../assets/TERRA.png'
 import TheWayFd from '../../assets/theWay-Fd.png'
 import AIchatbot from "../../assets/AIchatbot.png";
 import dataStrategist from '../../assets/dataStrategist.png';
-
+import maxup from '../../assets/maxup.png';
 
 const projectsData = [  
+   {
+    image: maxup,
+    title: 'MaXup',
+    desc: 'Maxup stands out as your go-to fitness web app due to its user-friendly interface, extensive exercise library, personalized approach, and robust tracking capabilities. Whether you are aiming to lose weight, build muscle, improve flexibility, or simply adopt a healthier lifestyle, FitFusion empowers you to reach your fitness goals effectively and sustainably.',
+    url: 'https://workout-client-seven.vercel.app/',
+    code: 'https://github.com/edwardogheneochuko/workout-client.git',
+    category: 'Projects',
 
+   },
   {
     image: AIchatbot,
     title: "Fevai Ai-chatbot",
     desc: "An AI-driven chat interface developed from a UI/UX Figma prototype, providing real-time, context-aware responses to enhance user interaction and accessibility.",
     url: "https://eden-tech-theta.vercel.app/",
-    code: "",
+    code: "https://eden-tech-theta.vercel.app/",
     category: "Projects"
   },
   {
@@ -21,7 +29,7 @@ const projectsData = [
     title: "The Way Conference",
     desc: "A fullstack conference registration application",
     url: "https://theway-mu.vercel.app/",
-    code: "",
+    code: "https://github.com/edwardogheneochuko/fols-conference-frontend.git",
     category: "Projects"
   },
 
@@ -30,7 +38,7 @@ const projectsData = [
     title: "Data Strategist",
     desc: "A fullstack conference specializing in leveraging data insights to drive growth and innovation for market and consumer brands.",
     url: "https://data-strategist.netlify.app/",
-    code: "",
+    code: "https://github.com/edwardogheneochuko/AI-DataStrategist.git",
     category: "Projects"
   },
 
@@ -39,7 +47,7 @@ const projectsData = [
     title: "Terra",
     desc: "A fullstack blockchain-based platform for creating and managing decentralized sales of agricultural produces.",
     url: "https://terra-sage.vercel.app/",
-    code: "",
+    code: "https://github.com/edwardogheneochuko/TerraTrust.git",
     category: "Projects"
   },
   
